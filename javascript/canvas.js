05a4b243dd26aa447af62783518b573c3101e92b
Singabe Djonouma <singabe-xd@hotmail.fr>
2023-03-08 21:37:28 +0100

    iteration test

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -6,10 +6,25 @@ class HangmanCanvas {
 
   createBoard() {
     // ... your code goes here
+    this.context.clearRect(0, 0, 800, 1200);
+    this.drawLines()
   }
 
   drawLines() {
     // ... your code goes here
+
+    hangman.secretWord
+    
+    this.context.beginPath()
+    // hangman.moveTo(500,700)
+    // hangman.lineTo(250, 50);
+    // hangman.stroke();
+    this.context.moveTo(250, 50);
+// draw the line that has final coordinates x=250, y=100
+this.context.lineTo(250, 100);
+// .stroke() executes the drawing
+this.context.stroke();
+
   }
 
   writeCorrectLetter(index) {
